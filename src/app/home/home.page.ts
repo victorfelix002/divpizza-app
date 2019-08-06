a0f20aff27cb004981ebd70f33ce836dd72ceba8
victor felix <felixvictor42@gmail.com>
2019-08-05 22:27:11 -0300

    criação de pagina para adicionar novas pizzas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  catalogo:Array<object> = []
 
-  constructor() {}
-
+  ionViewDidEnter() {
+    console.log('EXECUTOU O VIEW DID ENTER');
+      this.catalogo.push({
+      nome: 'Pepperoni lombado com queijo grudante',
+      descricao: 'Pepperoni, lombo, bastante queijo muçarela, cogumelos e um leve toque de azeite trufado e azeitonas.',
+      preco: 'R$100,00'
+    })
+  }
 }
